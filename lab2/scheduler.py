b6e85ece7125e828073ec0447cdfb0e660b88398
Saumitra Tiwari <satiwari@calpoly.edu>
2024-05-09 23:09:25 -0700

    updated the SRTN algorithm

--- lab2/scheduler.py
+++ lab2/scheduler.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 
 class Jobs:
     def __init__(self, run_time, arrival_time):
@@ -59,7 +60,7 @@ def FIFO_scheduler():
     print("Average turnaround time: ", average_turnaround_time)
 
 
-def SRTN_scheduler():
+def SJN_scheduler():
     global jobs
 
     for job in jobs:
@@ -110,6 +111,69 @@ def SRTN_scheduler():
         print("job number: ", job.job_number, "job burst time: ", job.run_time, "Job arrival time", job.arrival_time)
 
 
+def SRTN_scheduler():
+    global jobs
+
+    # sort the job based on the arrival time and if they have same arrival time sort it based on their run time
+    jobs.sort(key=lambda x: (x.arrival_time, x.run_time))
+
+    for job in jobs:
+        print("job number: ", job.job_number, "job burst time: ", job.run_time, "Job arrival time", job.arrival_time)
+
+    print("\n\n\n")
+
+    # temp copy of jobs
+    temp_jobs = copy.deepcopy(jobs)
+
+    timer = jobs[0].arrival_time
+    current_job = jobs[0]
+
+    # to calculate turnaround and wait time:
+    turn_around_time = 0
+    wait_time = 0
+    total_turn_around_time = 0
+    total_wait_time = 0
+    
+    while(len(jobs) > 0): # if there are jobs in the list
+
+        # first check if there is a job whose arrival time = timer && run-time < curr_job.run_time
+        for jb in jobs:
+            if(jb.arrival_time <= timer and current_job.run_time > jb.run_time):
+                current_job = jb
+        
+        # now we will check if curr_job run time is greater than 0
+        if(current_job.run_time > 0):
+            timer += 1  #increament timer
+            current_job.run_time -= 1 #decrement the run_time of current job
+
+        current_job_index = 0
+
+        # get the current index of the job on list
+        for jb in jobs:
+            if(current_job.job_number == jb.job_number):
+                current_job_index = jobs.index(jb)
+                break
+        
+        # update the job in the list based on the current job
+        if(current_job.run_time == 0):
+            turn_around_time = timer - current_job.arrival_time
+            total_turn_around_time += turn_around_time
+
+            for jb in temp_jobs:
+                if(current_job.job_number == jb.job_number):
+                    wait_time = turn_around_time - jb.run_time
+                    total_wait_time += wait_time
+                    break
+            
+            print("Job %3d -- Turnaround %3.2f  Wait %3.2f" % (current_job.job_number, turn_around_time, wait_time))
+
+            jobs.pop(current_job_index)
+            if(len(jobs) > 0):
+                current_job = jobs[0] # update the current job
+        else:
+            jobs[current_job_index] = current_job
+    
+    print("Average -- Turnaround %.2f  Wait %.2f" % (total_turn_around_time/len(temp_jobs), total_wait_time/len(temp_jobs)))
 
 
 
@@ -138,8 +202,10 @@ def main():
 
     if(algorithm == "FIFO"):
         FIFO_scheduler()
-    elif(algorithm == "SRTN"):
+    elif(algorithm == "SRTN"): # preamptive version
         SRTN_scheduler()
+    elif(algorithm == "SJN"):  # non-preamptive version
+        SJN_scheduler()
 
 
 
