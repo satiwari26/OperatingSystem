4e293eeabd2444f4472d4501327d6ec545d9068d
Junior Perez <perezw.junior@gmail.com>
2024-06-04 17:28:14 -0700

    doing tinyfs

--- lab4/tinyFS.cpp
+++ lab4/tinyFS.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <stdio.h>
 #include "libDisk.h"
+#include "tinyFS.h"
 
 using namespace std;
 
 int main(){
     char * t;
     printf("Hello world!\n");
+}
+
+int tfs_mkfs(char *filename, int nBytes)
+{
+    
 }
