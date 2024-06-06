34a6191477909d45945bf4ff5d0f7c27e2424fc2
Junior Perez <perezw.junior@gmail.com>
2024-06-06 02:22:03 -0700

    Added tests for mounting tfs

--- lab4/test.cpp
+++ lab4/test.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     printf("Hello world\n");
 
-    int result = tfs_mkfs((char*)"disk1", DEFAULT_DISK_SIZE);
-    cout << "result is " << result << endl;
-    //tfs_mount("disk1")
+    char *diskFileName = (char*) "disk1";
+    if(argc > 1)
+    {
+        diskFileName = argv[1];
+    }
+    // int mkfs_result = tfs_mkfs((char*)"disk1", DEFAULT_DISK_SIZE);
+    // cout << "mkfs result is " << mkfs_result << endl;
+
+
+    int mount_result = tfs_mount(diskFileName);
+    cout << "mount result is " << mount_result << endl;
+
+    return 0;
 }
