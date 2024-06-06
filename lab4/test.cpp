38cd7b2108aa4af4cbc7c613a1779f9d694f3757
Junior Perez <perezw.junior@gmail.com>
2024-06-06 01:49:39 -0700

    Changed test file and disk1 now holds updated contents when calling mkfs

--- lab4/test.cpp
+++ lab4/test.cpp
@@ -1,6 +1,12 @@
 #include "tinyFS.h"
 
+using namespace std;
+
 int main()
 {
     printf("Hello world\n");
+
+    int result = tfs_mkfs((char*)"disk1", DEFAULT_DISK_SIZE);
+    cout << "result is " << result << endl;
+    //tfs_mount("disk1")
 }
